d327111c9b9c1d6b90677a5d7286e50c00a2f9d4
robot <2466217976@qq.com>
2019-09-14 20:01:22 +0800

    button events finished

--- app/src/main/java/com/example/casper/ButtonMainActivity.java
+++ app/src/main/java/com/example/casper/ButtonMainActivity.java
@@ -3,12 +3,36 @@ package com.example.casper;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class ButtonMainActivity extends AppCompatActivity {
 
+    private Button buttonHelloEn,buttonHelloCz;
+    private TextView textViewHelloWorld;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_button_main);
+
+        buttonHelloEn=findViewById(R.id.buttonHelloEn);
+        buttonHelloCz=findViewById(R.id.buttonHelloCz);
+        textViewHelloWorld=findViewById(R.id.textViewHelloWorld);
+
+        buttonHelloCz.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textViewHelloWorld.setText(buttonHelloCz.getText());
+            }
+        });
+        buttonHelloEn.setOnClickListener(new ButtonOnClick());
+    }
+
+    private class ButtonOnClick implements View.OnClickListener {
+        @Override
+        public void onClick(View view) {
+            textViewHelloWorld.setText(((Button)view).getText());
+        }
     }
 }
